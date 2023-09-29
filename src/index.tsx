a4dd6169eb138e518aa00ba034f845d0f879edb1
Logan Hendershot <logan@lvx3.com>
2023-09-29 14:13:45 -0700

    fix(ui): added app.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import { createRoot } from "react-dom/client"
+import "/src/assets/App.css"
+import App from "components/App"
 
-const container = document.getElementById('root') as HTMLDivElement
+const container = document.getElementById("root") as HTMLDivElement
 const root = createRoot(container)
 
 root.render(<App />)
